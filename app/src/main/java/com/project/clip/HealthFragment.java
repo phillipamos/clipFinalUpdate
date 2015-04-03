c00c86dc5689d3e36334b80239616e16f82e1cf4
landry4678 <landryjoel2002@yahoo.fr>
2015-04-02 22:03:25 -0500

    hh

--- app/src/main/java/com/project/clip/HealthFragment.java
+++ app/src/main/java/com/project/clip/HealthFragment.java
@@ -1,9 +1,11 @@
 package com.project.clip;
 
 import android.app.AlertDialog;
+import android.app.Dialog;
 import android.content.DialogInterface;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
+import android.text.Editable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -30,8 +32,14 @@ public class HealthFragment extends Fragment  {
                              Bundle savedInstanceState) {
 
         View rootView = inflater.inflate(R.layout.fragment_health, container, false);
+
+        //blood Pressure image
         ImageView imageView = (ImageView) rootView.findViewById(R.id.bloodPressureIcon);
         addBloodPressureView(imageView);
+
+        //heart rate image
+        ImageView imageViewHeartRate = (ImageView) rootView.findViewById(R.id.HeartRateIcon);
+        addHeartRate(imageViewHeartRate);
 
         return rootView;
     }
@@ -48,33 +56,36 @@ public class HealthFragment extends Fragment  {
 
 
     }
+    void addHeartRate(ImageView imageView)
+    {
+        imageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ShowHeartRate(R.layout.diaglog_heartrate);
+
+            }
+        });
+    }
 
 
-
-    void ShowBloodPressureAlert(int layoutID) {
+   //blood pressure pop up
+void ShowBloodPressureAlert(int layoutID) {
 
         AlertDialog.Builder alert = new AlertDialog.Builder(getActivity());
 
         alert.setView(layoutID);
 
-       View v = getView();
-
-        EditText dia = (EditText)getView().findViewById(R.id.EditViewDiastolic);
-
-        EditText sys = (EditText)getView().findViewById(R.id.editViewSystolic);
-
-
         alert.setPositiveButton("Save", new DialogInterface.OnClickListener() {
             @Override
             public void onClick(DialogInterface dialog, int which) {
+               Dialog f = (Dialog)dialog;
+                EditText dia = (EditText)f.findViewById(R.id.EditViewDiastolic);
 
-
+                EditText sys = (EditText)f.findViewById(R.id.editViewSystolic);
                 TextView bp = (TextView) getView().findViewById(R.id.textViewBP);
-                //bp.setText(dia.getText().toString().trim());
-
-
-
-            }
+                Editable  sufo = dia.getText();
+                bp.setText(dia.getText().toString().trim() + "/" + sys.getText().toString().trim());
+     }
         });
 
         alert.setNegativeButton("Cancel",new DialogInterface.OnClickListener() {
@@ -87,6 +98,26 @@ public class HealthFragment extends Fragment  {
 
 
     }
+  void ShowHeartRate(int LayoutId)
+  {
+      AlertDialog.Builder alert = new AlertDialog.Builder(getActivity());
+      alert.setView(LayoutId);
+      alert.setPositiveButton("Save", new DialogInterface.OnClickListener() {
+          @Override
+          public void onClick(DialogInterface dialog, int which) {
+              Dialog f = (Dialog)dialog;
 
+          }
+      });
+
+      alert.setNegativeButton("Cancel",new DialogInterface.OnClickListener() {
+          @Override
+          public void onClick(DialogInterface dialog, int which) {
+
+          }
+      });
+      alert.show();
+
+  }
 
 }
